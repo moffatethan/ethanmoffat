f2883d435809cf9277163ecf610ecd38715fc54b
Ethan Moffat <ethanmoffat@hey.com>
2021-05-13 03:43:18 -0700

    fixed tailwind config bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './shared/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
