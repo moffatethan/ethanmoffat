31f95428dabbc870d722ef2850e623efee610c45
Ethan Moffat <ethanmoffat@hey.com>
2021-05-13 03:21:39 -0700

    tailwind config work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [
-    // This is not present inside the default configuration
-    // but it's good to build your production application
-    // Read more about this here: https://tailwindcss.com/docs/installation#building-your-css
-    './pages/**/*.js',
-  ],
+  purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
