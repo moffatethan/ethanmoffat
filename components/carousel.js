06222414f4a14520641eb2fc9319e47570bb39aa
Ethan Moffat <ethanmoffat@hey.com>
2021-05-14 18:07:53 -0700

    removed shared folder in tailwind config and added metadata to project carousel

--- components/carousel.js
+++ components/carousel.js
@@ -12,22 +12,28 @@ const ReactCarousel = dynamic(
   }
 )
 
-const ProjectCarouselSlide = ({ logoUrl, projectImageUrl, description }) => (
-  <div className='flex-col lg:flex-row flex bg-white dark:bg-gray-200 shadow-lg rounded-3xl p-8'>
-    <div className='flex-1 px-5'>
-      <img className='w-96' src={logoUrl} />
-      <p className='font-body text-xl text-gray-600 mt-5 leading-10 line-clamp-5'>
-        {description}
-      </p>
-      <button className='mt-12 bg-blue-500 border shadow-md border-blue-500 text-blue-100 text-md px-12 py-4 rounded-lg transition hover:bg-blue-600 transform hover:-translate-y-1'>View Project</button>
+const ProjectCarouselSlide = ({ logoUrl, projectImageUrl, description, usedTech, isConcept }) => (
+  <div className='bg-white dark:bg-gray-200 shadow-lg rounded-3xl p-8'>
+    <div className='flex px-5'>
+      <div>
+        <img className='w-96' src={logoUrl} />
+        <p className='font-body text-xl text-gray-600 mt-5 leading-10 line-clamp-5'>
+          {description}
+        </p>
+        <div className="mt-12 mr-8 text-xl text-gray-400 font-medium leading-10">
+          <p className="mb-4">Tech Used: {usedTech.join(", ")}</p>
+          <p>Concept: {isConcept ? 'Yes' : 'No' }</p>
+        </div>
+      </div>
+      <img className='hidden lg:block flex-1 h-96 shadow-md rounded-3xl' src={projectImageUrl} />
     </div>
-    <img className='hidden lg:block flex-1 h-96 shadow-md rounded-3xl' src={projectImageUrl} />
+    <button className='w-full md:w-auto mt-12 bg-blue-500 border shadow-md border-blue-500 text-blue-100 text-md px-12 py-4 rounded-lg transition hover:bg-blue-600 transform hover:-translate-y-1'>View Project</button>
   </div>
 )
 
 const CarouselSlide = ({ imgUrl, title, description, small = true }) => (
   <div className="bg-white dark:bg-gray-200 shadow-lg rounded-3xl px-8 py-6">
-    <div className='block md:flex h-48'>
+    <div className='block md:flex md:h-48'>
       <img src={imgUrl} className="md:w-56 mb-4 md:mb-0 rounded-3xl" />
       <div className='md:ml-8 max-h-36 md:text-left text-center cursor-default'>
         <h3 className='text-3xl font-medium text-gray-700 line-clamp-2 md:line-clamp-3'>{title}</h3>
@@ -84,7 +90,7 @@ export const Carousel = ({ slides, arrows = true, size = 'small' }) => {
                 {
                   size === 'small'
                   ? <CarouselSlide imgUrl={slide.postImage?.url} title={slide.title} description={slide.description} small={true} />
-                  : <ProjectCarouselSlide logoUrl={slide.projectLogo?.url} title={slide.title} description={slide.description} projectImageUrl={slide.projectImage?.url} small={false} />
+                  : <ProjectCarouselSlide logoUrl={slide.projectLogo?.url} title={slide.title} description={slide.description} projectImageUrl={slide.projectImage?.url} small={false} isConcept={slide.isConcept} usedTech={slide.metadata.techUsed} />
                 }
               </div>
             )
